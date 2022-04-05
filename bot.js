12c1b078a171dae87375aa02e444007e83c086cc
mmmmmmmmak <88006224+mmmmmmmmak@users.noreply.github.com>
2022-04-05 15:01:08 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,131 @@
+const { HearManager } = require('@vk-io/hear');
+const { QuestionManager } = require('vk-io-question');
+const {VK, resolveResource, API, getRandomId, Keyboard, MessageForwardsCollection} = require('vk-io');
+const { post } = require('request');
+
+const vk = new VK({
+    token: '64d54def091abf4c3b1d24341e258f5b701ca40156edc232a9264b0639affa706f97bbb33459c4dfb872c'
+    // token: 'b79d783748e89e89eaef264a76e1c142702c66f6cd24bd8c3258f2da4015af94dc6fe8678aebc14cb94a7'
+});
+
+const { api, updates, snippets } = vk
+
+const questionManager = new QuestionManager();
+const cmd = new HearManager();
+const commands = [];
+
+vk.updates.use(questionManager.middleware);
+vk.updates.on('message_new' , cmd.middleware)
+
+cmd.hear(/^hello$/, async (context) => {
+await context.send('Hello!');
+});
+
+console.log('Старт')
+vk.updates.start()
+
+async function getVkNameById(id) {
+    const [data] = await api.users.get({
+      user_ids: id,
+    });
+    return data.first_name;
+  } 
+
+  let groups = ['https://vk.com/almazovyt', 'https://vk.com/ivansmedia']
+  let groupsnum = [129400026, 211145670]
+
+  cmd.hear(/^(?:Начать)$/i, async (msg) => {
+    const name = await getVkNameById(msg.senderId) /*nd*/
+    if (!msg.isChat) {
+        msg.send(
+            `Привет, ${name}, вот мои функции:`,
+            { 
+                keyboard: JSON.stringify( 
+                { 
+                "one_time": false,
+                "buttons": [ 
+                [{ 
+                "action": { 
+                "type": "text", 
+                "payload": "{\"button\": \"1\"}", 
+                "label": "Дай мне ссылки для подписки!" 
+                }, 
+                "color": "positive" 
+                }],
+            ]
+            })
+        }
+        )
+    }
+})
+
+cmd.hear(/^(?:Дай мне ссылки для подписки!)$/i, async (msg) => {
+    const name = await getVkNameById(msg.senderId) /*nd*/
+    if (!msg.isChat) {
+        msg.send(
+            `Ещё раз привет, ${name}, лови ссылочки: ${groups.join('\n')}`
+            ,
+            { 
+                keyboard: JSON.stringify( 
+                { 
+                "one_time": false,
+                "buttons": [ 
+                [{ 
+                "action": { 
+                "type": "text", 
+                "payload": "{\"button\": \"1\"}", 
+                "label": "Я подписался на все ссылки." 
+                }, 
+                "color": "positive" 
+                }],
+            ]
+            })
+        })
+    }
+})
+
+cmd.hear(/^(?:добавить группу)$/i, async (msg) => {
+    if (!msg.isChat) {
+        const answ = await msg.question(`Введите полную ссылку группы (формат https://vk.com/123):`)
+        if(answ.text.includes(`https://vk.com`)) {
+             msg.send(`Отлично! Полная ссылка добавлена в массив.`)
+             groups.push(answ.text)
+             const answer = await msg.question(`Введите цифры от начального айди группы (получить можно здесь: https://regvk.com/id/):`)
+                if (/[a-zA-Z]/.test(answer.text)){
+                    msg.send(`Формат неверный! Выполните данную команду снова.`)
+                }
+                else {
+                    groupsnum.push(Number(answer.text))
+                return msg.send(`Всё получилось! Цифровое айди добавлено в список.`)
+        }
+        }
+        else if(!answ.text.includes(`https://vk.com`)) {
+            return msg.send(`Формат ссылки неверный. Повторите данную команду снова!`)
+        }
+    }
+})
+
+cmd.hear(/^(?:проверить группы)$/i, async (msg) => {
+    if (!msg.isChat) {
+        const answer = await msg.question(`Введите цифры от начального айди пользователя (получить можно здесь: https://regvk.com/id/):`)
+        let otpr = 0
+        let unsub = 0
+        groupsnum.forEach((item, index, array) => {
+            vk.api.groups.isMember({
+            user_id: answer.text,
+            group_id: item,
+        }).then(function(res) {
+            console.log(res)
+            if(res < 1) {
+                unsub = 1
+                msg.send(`Ошибка! Пользователь не подписался на группу: vk.com/club${item}.`)
+            }
+            else if (res > 0) {
+                if(otpr == 0 && unsub == 0) {
+                    otpr = 1
+                    return msg.send(`Проверяю... (если я не напишу об ошибке, то все проверки пройдены!)`)
+                }
+            }
+        })})
+    }
+})
