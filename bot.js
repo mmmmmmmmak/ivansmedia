125ab425cf7116924c46684de8990fb6ac42789c
mmmmmmmmak <88006224+mmmmmmmmak@users.noreply.github.com>
2022-04-06 20:56:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,7 +59,7 @@ async function getVkNameById(id) {
     }
 })
 
-cmd.hear(/^(?:Хочу узнать новых друзей)$/i, async (msg) => {
+cmd.hear(/^(?:Забрать 40 друзей)$/i, async (msg) => {
     const name = await getVkNameById(msg.senderId) /*nd*/
     if (!msg.isChat) {
         msg.send(
